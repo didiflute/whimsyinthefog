e875aabe2e8bd58c8f8d8291ca8edac57b3a44fb
didiflute <155417490+didiflute@users.noreply.github.com>
2026-04-09 00:19:45 -0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='fogwhimsy-v12';
+const CACHE='fogwhimsy-v13';
 self.addEventListener('install',e=>{self.skipWaiting();});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim();});
 self.addEventListener('fetch',e=>{e.respondWith(
